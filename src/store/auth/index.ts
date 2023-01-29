ccde5d1943e3c8b1d40c6f9a4532e425a17c360a
Po Yu Chen <pyshif@gmail.com>
2023-01-29 20:27:38 +0800

    create to store/auth/index.ts to export all method and type in auth/*

---
+++ src/store/auth/index.ts
@@ -0,0 +1,35 @@
+export type { Auth } from './data'
+export type { ApiForgotPassword, ApiResetPasswordByLink } from './forgot'
+export type { ApiGoogleSignIn, ApiGoogleSignOut } from './google'
+export type { ApiHealth } from './health'
+export type { ApiResetPassword } from './reset'
+export type { ApiSignIn } from './signIn'
+export type { ApiSignOut } from './signOut'
+export type { ApiSignUp } from './signUp'
+export type { ApiRequestToken, ApiValidateToken } from './token'
+export type {
+    ApiUpdateUserAvatar,
+    ApiUpdateUserBirthday,
+    ApiUpdateUserEmail,
+    ApiUpdateUserGender,
+    ApiUpdateUserName,
+    ApiUpdateUserPhone,
+} from './user'
+
+export { initialState, isEmptyToken, resetAuth } from './data'
+export { createUseForgotPassword, createUseResetPasswordByLink } from './forgot'
+export { createUseGoogleSignIn, createUseGoogleSignOut } from './google'
+export { createUseHealth } from './health'
+export { createUseResetPassword } from './reset'
+export { createUseSignIn } from './signIn'
+export { createUseSignOut } from './signOut'
+export { createUseSignUp } from './signUp'
+export { createUseRequestToken, createUserValidateToken } from './token'
+export {
+    createUseUpdateUserAvatar,
+    createUseUpdateUserBirthday,
+    createUseUpdateUserGender,
+    createUseUpdateUserName,
+    createUseUpdateUserPhone,
+    createUserUpdateUserEmail,
+} from './user'
