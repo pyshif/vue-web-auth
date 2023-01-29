2b431e6f302760310dc0656ae6ae8938b50ce328
Po Yu Chen <pyshif@gmail.com>
2023-01-29 20:42:31 +0800

    remove api/index.ts UpdateUserAvatar type

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,3 @@
-
 import axios from 'axios'
 import * as Forgot from './auth/forgot'
 import * as GSI from './auth/gsi'
@@ -18,14 +17,13 @@ export type { ResetPasswordData } from './auth/reset'
 export type { SignInData, SignInResponseData } from './auth/signin'
 export type { SignUpData } from './auth/signup'
 export type { RequestTokenResponseData } from './auth/token'
-export type { UpdateUserAvatarData } from './auth/user'
 
 const instance = axios.create({
-    baseURL: import.meta.env.API_URL || 'http://localhost:3003/'
-});
+    baseURL: import.meta.env.API_URL || 'http://localhost:3003/',
+})
 
-export const apiForgotPassword = Forgot.ForgotPassword(instance);
-export const apiResetPasswordByLink = Forgot.ResetPasswordByLink(instance);
+export const apiForgotPassword = Forgot.ForgotPassword(instance)
+export const apiResetPasswordByLink = Forgot.ResetPasswordByLink(instance)
 export const apiGoogleSignIn = GSI.GoogleSignIn(instance)
 export const apiGoogleSignOut = GSI.GoogleSignOut(instance)
 export const apiHealth = Health.Health(instance)
@@ -36,7 +34,7 @@ export const apiSignUp = SignUp.SignUp(instance)
 export const apiVaildateEmailAddress = SignUp.ValidateEmailAddress(instance)
 export const apiValidateToken = Token.ValidateToken(instance)
 export const apiRequestToken = Token.RequestToken(instance)
-export const apiDeleteUser = User.DeleteUser(instance);
+export const apiDeleteUser = User.DeleteUser(instance)
 export const apiUpdateUserAvatar = User.UpdateUserAvatar(instance)
 export const apiUpdateUserBirthday = User.UpdateUserBirthday(instance)
 export const apiUpdateUserEmail = User.UpdateUserEmail(instance)
