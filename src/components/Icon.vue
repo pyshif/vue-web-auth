431e0738a9cc8839d99e46411533c08f3cd7c00f
Po Yu Chen <pyshif@gmail.com>
2023-02-05 19:51:29 +0800

    complete Icon and IconButton component

---
+++ src/components/Icon.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+export interface Props {
+    fontawesome: string
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+    <i :class="props.fontawesome"></i>
+</template>
---
+++ src/components/IconButton.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import Icon from '@/components/Icon.vue'
+
+export interface Props {
+    fontawesome: string
+}
+
+const props = defineProps<Props>()
+
+// FIXME: How to prompt user must to pass @click ?
+</script>
+
+<template>
+    <button>
+        <Icon :fontawesome="props.fontawesome" />
+    </button>
+</template>
+
+<style scoped>
+button {
+    background-color: ivory;
+    color: var(--color-text-dark);
+    font-size: 1rem;
+    border: 0;
+    transition: 0.4s;
+    cursor: pointer;
+}
+
+button:hover {
+    transform: translateX(-1%) translateY(-1%);
+    filter: brightness(1.5);
+}
+
+button:focus-visible {
+    outline: none;
+}
+</style>
