32e05817bf475d4b0b9bc5c16c79dfa7c046e080
Po Yu Chen <pyshif@gmail.com>
2023-02-06 01:36:38 +0800

    complete IconLink component

---
+++ src/components/IconLink.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Icon from '@/components/Icon.vue'
+import Link from '@/components/Link.vue'
+
+export interface Props {
+    to: string
+    fontawesome: string
+}
+
+const { to, fontawesome } = defineProps<Props>()
+</script>
+
+<template>
+    <Link :to="to">
+        <Icon :fontawesome="fontawesome" class="icon-link" />
+        <slot></slot>
+    </Link>
+</template>
+
+<style>
+/* 50% -> 1:1 */
+.icon-link:before {
+    margin-right: 20%;
+}
+</style>
