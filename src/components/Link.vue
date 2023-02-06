0a8fd40d1f36b93a812e56e22efac3a9032ac410
Po Yu Chen <pyshif@gmail.com>
2023-02-06 19:32:51 +0800

    complete Link component

---
+++ src/components/Link.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+export interface Props {
+    to: string
+}
+const { to } = defineProps<Props>()
+</script>
+
+<template>
+    <RouterLink :to="to" class="vue-link">
+        <slot></slot>
+    </RouterLink>
+</template>
+
+<style>
+.vue-link {
+    text-decoration: none;
+    transition: 0.4s;
+    color: var(--color-text-dark);
+}
+
+.vue-link:hover {
+    filter: brightness(1.5);
+}
+</style>
